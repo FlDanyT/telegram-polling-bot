ae8fb69d689a5fca6bd9dc2684c034f3f05084bd
FlDanyT <96938294+FlDanyT@users.noreply.github.com>
2022-11-02 12:57:33 +0300

    Update db.js

--- db.js
+++ db.js
@@ -3,12 +3,12 @@ const {Sequelize} = require('sequelize')
   
 module.exports = new Sequelize(
 
-'bot_tgg', // Имя базы данных
-'root', // Логин
-'-rY&<kbjd88',  // Пароль от базы
+'', // Имя базы данных
+'', // Логин
+'',  // Пароль от базы
 {
-        host: '127.0.0.1', // Хост
-        port: '3306', // Порт
-        dialect: 'mysql' // База
+        host: '', // Хост
+        port: '', // Порт
+        dialect: '' // База
 }      
 )
